c268918aa12a683ee817905644aaa10d3c8e8daa
AbdulHannan-123 <ah6475774@gmail.com>
2023-03-01 01:05:37 +0500

    30% properties of text_field_widget

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,52 @@ class TextFieldWidgets extends StatefulWidget {
 }
 
 class _TextFieldWidgetsState extends State<TextFieldWidgets> {
+
+  final _controller = TextEditingController();
+  var _countertext = "";  // how many characters user has entered
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title:const Text("TEXT_FIELD_WIDGET"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              color: Colors.grey,
+              padding: EdgeInsets.all(8),
+              child: TextField(
+
+                controller: _controller,       // the value you typed is stored here
+
+                onChanged: (value) {          // notify each strike value
+                  setState(() {
+                    // _countertext = (10 - value.length).toString();   // limit the character to be enter in the textfield
+                  });
+                },
+
+                // maxLength: 10,      // its is the max charector length you chan enter
+
+                // autocorrect: true,           // auto correct and auto complete the world
+
+                // autofillHints: ,   // it req the list of ristricted words to auto complete fill in iterable form
+                
+                // autofocus: true,    // to get the keyboard focus
+
+                // buildCounter: ,
+               
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Text(_controller.text,textAlign: TextAlign.center,),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
